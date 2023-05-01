cb3124639e6e89f4576a81d54fe2b14c8844c0c6
Andre Kuznetcov <andre.kuzac@gmail.com>
2023-05-01 06:44:40 +0100

    Added g4, metas, links to resources

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/edge";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
+  integrations: [
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="astro/client" />
+
+declare global {
+  interface Window {
+    dataLayer: (string | Date)[];
+  }
+}
+
+export {};
