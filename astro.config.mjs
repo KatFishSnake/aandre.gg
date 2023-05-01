80a475e297518fc463749695cea73ed872f1ccf8
Andre Kuznetcov <andre.kuzac@gmail.com>
2023-05-01 09:09:48 +0100

    Optimized

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/edge";
 import partytown from "@astrojs/partytown";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,5 +14,6 @@ export default defineConfig({
         forward: ["dataLayer.push"],
       },
     }),
+    tailwind(),
   ],
 });
